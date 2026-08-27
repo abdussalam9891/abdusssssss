4779cf09bf8b1f25aa2a8af2eb13b71f453830b1
abdussalam9891 <abdussalam9891@gmail.com>
2026-08-27 19:13:57 +0530

    feat(contact): add icon badges to WhatsApp/Email/Phone rows, fix Banshiwaale typo

--- src/components/contact/contactForm.js
+++ src/components/contact/contactForm.js
@@ -514,7 +514,7 @@ export function createContactForm(contactInfo = {}) {
               text-[#A07936]
             "
           >
-            Contact Banshiwaale
+            Contact Banshiwale
           </span>
 
 
@@ -592,43 +592,78 @@ export function createContactForm(contactInfo = {}) {
 
             class="
               group
-              block
+              flex
+              items-start
+              gap-4
             "
           >
 
-            <span
+            <div
               class="
-                text-[11px]
-                font-semibold
-                uppercase
-                tracking-[0.18em]
+                flex
+                h-10
+                w-10
+                shrink-0
+                items-center
+                justify-center
+
+                rounded-full
+
+                border
+                border-white/10
 
                 text-[#A07936]
               "
             >
-              WhatsApp
-            </span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 32 32"
+                fill="currentColor"
+                class="h-5 w-5"
+              >
+                <path d="M16 3C8.83 3 3 8.72 3 15.78c0 2.54.74 4.99 2.14 7.11L3.5 29l6.32-1.61A13.1 13.1 0 0 0 16 28.56c7.17 0 13-5.72 13-12.78S23.17 3 16 3Zm0 23.12a10.9 10.9 0 0 1-5.57-1.53l-.4-.24-3.75.95 1-3.63-.26-.42a10.53 10.53 0 0 1-1.63-5.47C5.39 10.1 10.06 5.5 16 5.5s10.61 4.6 10.61 10.28S21.94 26.12 16 26.12Zm5.88-7.74c-.32-.16-1.9-.93-2.2-1.04-.29-.1-.5-.16-.71.16s-.82 1.03-1 1.24c-.19.21-.37.24-.69.08-.32-.16-1.34-.49-2.55-1.56-.94-.83-1.57-1.86-1.76-2.18-.18-.31-.02-.48.14-.64.14-.14.32-.37.48-.56.16-.18.21-.31.32-.52.1-.21.05-.39-.03-.55-.08-.16-.71-1.69-.97-2.32-.26-.61-.53-.53-.71-.54h-.61c-.21 0-.55.08-.84.39-.29.31-1.11 1.08-1.11 2.63s1.13 3.05 1.29 3.26c.16.21 2.22 3.35 5.38 4.69.75.32 1.34.51 1.8.66.76.24 1.45.21 2 .13.61-.09 1.9-.78 2.17-1.54.27-.76.27-1.41.19-1.54-.08-.13-.29-.21-.61-.37Z"/>
+              </svg>
+
+            </div>
+
+            <div>
+
+              <span
+                class="
+                  text-[11px]
+                  font-semibold
+                  uppercase
+                  tracking-[0.18em]
+
+                  text-[#A07936]
+                "
+              >
+                WhatsApp
+              </span>
 
 
-            <span
-              data-contact-whatsapp-text
+              <span
+                data-contact-whatsapp-text
 
-              class="
-                mt-2
-                block
+                class="
+                  mt-2
+                  block
 
-                text-sm
+                  text-sm
 
-                text-white/80
+                  text-white/80
 
-                transition-colors
-                duration-300
+                  transition-colors
+                  duration-300
 
-                group-hover:text-white
-              "
-            >
-              ${whatsapp || phone || LOADING_TEXT}
-            </span>
+                  group-hover:text-white
+                "
+              >
+                ${whatsapp || phone || LOADING_TEXT}
+              </span>
+
+            </div>
 
           </a>
 
@@ -642,45 +677,85 @@ export function createContactForm(contactInfo = {}) {
 
             class="
               group
-              block
+              flex
+              items-start
+              gap-4
             "
           >
 
-            <span
+            <div
               class="
-                text-[11px]
-                font-semibold
-                uppercase
-                tracking-[0.18em]
+                flex
+                h-10
+                w-10
+                shrink-0
+                items-center
+                justify-center
+
+                rounded-full
+
+                border
+                border-white/10
 
                 text-[#A07936]
               "
             >
-              Email
-            </span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="h-5 w-5"
+              >
+                <path d="M4 5h16v14H4z" />
+                <path d="m4 6 8 7 8-7" />
+              </svg>
+
+            </div>
+
+            <div>
+
+              <span
+                class="
+                  text-[11px]
+                  font-semibold
+                  uppercase
+                  tracking-[0.18em]
+
+                  text-[#A07936]
+                "
+              >
+                Email
+              </span>
 
 
-            <span
-              data-contact-email-text
+              <span
+                data-contact-email-text
 
-              class="
-                mt-2
-                block
+                class="
+                  mt-2
+                  block
 
-                break-all
+                  break-all
 
-                text-sm
+                  text-sm
 
-                text-white/80
+                  text-white/80
 
-                transition-colors
-                duration-300
+                  transition-colors
+                  duration-300
 
-                group-hover:text-white
-              "
-            >
-              ${email || LOADING_TEXT}
-            </span>
+                  group-hover:text-white
+                "
+              >
+                ${email || LOADING_TEXT}
+              </span>
+
+            </div>
 
           </a>
 
@@ -694,43 +769,78 @@ export function createContactForm(contactInfo = {}) {
 
             class="
               group
-              block
+              flex
+              items-start
+              gap-4
             "
           >
 
-            <span
+            <div
               class="
-                text-[11px]
-                font-semibold
-                uppercase
-                tracking-[0.18em]
+                flex
+                h-10
+                w-10
+                shrink-0
+                items-center
+                justify-center
+
+                rounded-full
+
+                border
+                border-white/10
 
                 text-[#A07936]
               "
             >
-              Phone
-            </span>
+
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="h-5 w-5"
+              >
+                <path d="M6.6 10.8c1.4 2.7 3.6 4.9 6.3 6.3l2.1-2.1c.3-.3.7-.4 1-.2 1.1.4 2.3.6 3.5.6.6 0 1 .4 1 1V20c0 .6-.4 1-1 1C10.4 21 3 13.6 3 4.5c0-.6.4-1 1-1H7.6c.6 0 1 .4 1 1 0 1.2.2 2.4.6 3.5.1.4 0 .8-.2 1L6.6 10.8Z" />
+              </svg>
+
+            </div>
+
+            <div>
+
+              <span
+                class="
+                  text-[11px]
+                  font-semibold
+                  uppercase
+                  tracking-[0.18em]
+
+                  text-[#A07936]
+                "
+              >
+                Phone
+              </span>
 
 
-            <span
-              data-contact-phone-text
+              <span
+                data-contact-phone-text
 
-              class="
-                mt-2
-                block
+                class="
+                  mt-2
+                  block
 
-                text-sm
+                  text-sm
 
-                text-white/80
+                  text-white/80
 
-                transition-colors
-                duration-300
+                  transition-colors
+                  duration-300
 
-                group-hover:text-white
-              "
-            >
-              ${phone || LOADING_TEXT}
-            </span>
+                  group-hover:text-white
+                "
+              >
+                ${phone || LOADING_TEXT}
+              </span>
+
+            </div>
 
           </a>
 
@@ -760,7 +870,7 @@ export function createContactForm(contactInfo = {}) {
               text-[#A07936]
             "
           >
-            Banshiwaale Office
+            Banshiwale Office
           </span>
 
 
